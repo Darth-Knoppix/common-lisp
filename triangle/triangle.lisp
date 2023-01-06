22bf00c41b1a9ad060fb47951670535c2e4b6e37
Seth Corker <seth.corker@gmail.com>
2023-01-06 17:06:35 +0000

    complete triangle

--- triangle/triangle.lisp
+++ triangle/triangle.lisp
@@ -5,19 +5,25 @@
 (in-package :triangle)
 
 (defun triangle-type-p (type a b c)
-  (cond 
-    ((eq type :equilateral)
-     (cond (
-            ((= 0 a b c) nil)
-            ((= a b c) t)
-            (t nil)))
-     
-    ;; ((= type :isosceles)
-    ;;  (cond (
-    ;;         (() nil)
-    ;;  )))
-    ;; ((= type :scalene) ())
-    (t nil)
-     )
-  )
-)
+  (cond
+    ((not (triangle-equality a b c)) nil)
+    ((eq type :equilateral) (cond
+                              ((= a b c) t)
+                              (t nil)))
+
+    ((eq type :isosceles) (cond
+                              ((and (= b c)) t)
+                              ((and (= a b)) t)
+                              ((and (= a c)) t)
+                              (t nil)))
+    ((eq type :scalene) (cond
+                         ((/= a b c) t)
+                              (t nil)))
+    (t nil)))
+
+(defun triangle-equality (a b c)
+    (and
+     (not (= 0 a b c))
+     (>= (+ b c) a)
+     (>= (+ a c) b)
+     (>= (+ b a) c)))
