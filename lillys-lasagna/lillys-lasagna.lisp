f6ee0fb594c5df478f52158ea698939f74548e3f
Seth Corker <seth.corker@gmail.com>
2023-01-07 11:16:43 +0000

    complete lasagne

--- lillys-lasagna/lillys-lasagna.lisp
+++ lillys-lasagna/lillys-lasagna.lisp
@@ -8,9 +8,19 @@
 (in-package :lillys-lasagna)
 
 ;; Define function expected-time-in-oven
+(defun expected-time-in-oven  ()
+                              "Time in the oven in minuets"
+                              337)
 
 ;; Define function remaining-minutes-in-oven
+(defun remaining-minutes-in-oven  (elapsed)
+                                  "Remaining minutes in the oven given elapsed time"
+                                  (max 0 (- (expected-time-in-oven) elapsed)))
 
 ;; Define function preparation-time-in-minutes
+(defun preparation-time-in-minutes (layers) "Sum of 19 minuets per layer" (* layers 19))
 
 ;; Define function elapsed-time-in-minutes
+(defun elapsed-time-in-minutes  (layers elapsed)
+                                "Total for prep and elapsed time"
+                                (+ (preparation-time-in-minutes layers) elapsed))
