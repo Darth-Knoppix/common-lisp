9c203e3b167d371973d229daa23222365cd872ab
Seth Corker <seth.corker@gmail.com>
2023-01-09 09:53:48 +0000

    complete leap

--- leap/leap.lisp
+++ leap/leap.lisp
@@ -4,4 +4,21 @@
 (in-package :leap)
 
 (defun leap-year-p (year)
-  )
+  (if
+   (divisible-by-4 year)
+   (if
+    (divisible-by-100 year)
+    (if
+     (divisible-by-400 year)
+     T
+     nil)
+    T)
+   nil))
+
+(defun evenly-divisible (num div) (= 0 (mod num div)))
+
+(defun divisible-by-4 (num) (evenly-divisible num 4))
+
+(defun divisible-by-100 (num) (evenly-divisible num 100))
+
+(defun divisible-by-400 (num) (evenly-divisible num 400))
